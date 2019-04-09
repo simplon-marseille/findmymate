8cbfcb80275117789d1edcf68f05647681ba018a
simplon-marseille <acardnicolas@hotmail.com>
2019-04-09 12:02:48 +0200

    update pour une liste de codeuse impair

--- src/render.js
+++ src/render.js
@@ -43,14 +43,113 @@ const codeuse8 = {
   nom: "maryam",
   photo: "maryam.jpg"
 };
+
+const codeuse9 = {
+  nom: "fazia",
+  photo: "fazia.jpg"
+};
+
+const codeuse10 = {
+  nom: "florence",
+  photo: "florence.jpg"
+};
+
+const codeuse11 = {
+  nom: "gwenn",
+  photo: "gwennn.jpg"
+};
+
+const codeuse12 = {
+  nom: "helene",
+  photo: "hélène.jpg"
+};
+
+const codeuse13 = {
+  nom: "laurene",
+  photo: "laurene.jpg"
+};
+
+const codeuse14 = {
+  nom: "magali",
+  photo: "magali.jpg"
+};
+
+const codeuse15 = {
+  nom: "maria",
+  photo: "maria.jpg"
+};
+
+const codeuse16 = {
+  nom: "myriam",
+  photo: "myriam.jpg"
+};
+
+const codeuse17 = {
+  nom: "myriam",
+  photo: "myriam2.jpg"
+};
+
+const codeuse18 = {
+  nom: "nacira",
+  photo: "nacira.jpg"
+};
+
+const codeuse19 = {
+  nom: "niima",
+  photo: "niima.jpg"
+};
+
+const codeuse20 = {
+  nom: "romane",
+  photo: "romane.jpg"
+};
+
+const codeuse21 = {
+  nom: "tadikira",
+  photo: "tadikira.jpg"
+};
+
+const codeuse22 = {
+  nom: "tania",
+  photo: "tania.jpg"
+};
+
+const codeuse23 = {
+  nom: "yasmina",
+  photo: "yasmina.jpg"
+};
 //const findMyMate = require('./team');
 //
 
 
 
 
-//const promo = ["émilie", "aurelie","laurene", "sophie", "vanessa", "audray", "cathy", "chloé"];
-const promo = [codeuse1, codeuse2, codeuse3, codeuse4, codeuse5, codeuse6, codeuse7, codeuse8];
+const promo = [codeuse1,
+codeuse2,
+codeuse3,
+codeuse4,
+codeuse5,
+codeuse6,
+codeuse7,
+codeuse8,
+codeuse9,
+codeuse10,
+codeuse11,
+codeuse12,
+codeuse13,
+codeuse14,
+codeuse15,
+codeuse16,
+codeuse17,
+codeuse18,
+codeuse19,
+codeuse20,
+codeuse21,
+codeuse22,
+codeuse23
+];
+
+
 const promoLength = promo.length;
 
 const shuffle = (array) => {
@@ -87,7 +186,7 @@ const findMyMate = (promoShuffled) => {
   if(isEven(promoLength) === true){
   return promoShuffled;
 }else{
-  promoShuffled[2].push(promoShuffled[3]);
+  promoShuffled[promoShuffled.length-2].push(promoShuffled[promoShuffled.length-1][0]);
   promoShuffled.splice(promoShuffled.length-1, 1);
   return promoShuffled;
   };
